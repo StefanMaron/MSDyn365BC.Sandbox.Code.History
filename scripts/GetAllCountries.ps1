47b008909c6cda79df7aac7fe72f652a898b6419
Stefan Maron <stefanmaron@outlook.de>
2024-04-08 16:26:04 +0200

    asd

--- scripts/GetAllCountries.ps1
+++ scripts/GetAllCountries.ps1
@@ -2,7 +2,7 @@ $countries = [System.Collections.ArrayList]::new()
 Get-BCArtifactUrl -select All -Type Sandbox | % {
     [System.Uri]$Url = $_
     [version]$Version = $Url.AbsolutePath.Split('/')[2]
-    if ($Version -ge [version]::Parse('15.0.0.0')) {
+    if ($Version -ge [version]::Parse('23.5.0.0')) {
         $Url.AbsolutePath.Split('/')[3]
     } 
 }| Sort-object -unique | % { $countries.Add($_)}
