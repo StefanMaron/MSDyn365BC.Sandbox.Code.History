db2773eb454a1eb5d3e1e98e51024239c9857b93
Stefan Maron <stefanmaron@outlook.de>
2024-04-09 07:40:13 +0200

    asd

--- scripts/Auto_load_versions.ps1
+++ scripts/Auto_load_versions.ps1
@@ -42,7 +42,7 @@ $Versions | Sort-Object -Property Country, Version | % {
         }
 
         if ($Version.Major -gt 15 -and $Version.Build -gt 5) {
-            $CommitIDLastCUFromPreviousMajor = git log --all --grep="$($country)-$($version.Major - 1).5" --pretty=format:"%h"
+            $CommitIDLastCUFromPreviousMajor = git log --all -n 1 --grep="$($country)-$($version.Major - 1).5" --pretty=format:"%h"
         }
         else {
             $CommitIDLastCUFromPreviousMajor = $null
